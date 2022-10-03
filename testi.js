7b8e812586794f8261454f77d30330b54ea97be3
heikki-nieminen <heikki.nieminenn@gmail.com>
2022-10-03 15:34:10 +0300

    testi

--- testi.js
+++ testi.js
@@ -1,9 +1,43 @@
 const biggerThanHundred = (number) => {
-    number = 101
-    if (biggerThanHundred > 100) {
+    if (typeof number != 'number') {
+        return "Syötä luku"
+    }
+    else if (number > 100) {
         return "Syötit luvun, joka on suurempi kuin 100"
     }
     else {
         return ""
     }
 }
+
+console.log(biggerThanHundred(101))
+
+const toSeconds = (hours, minutes, seconds) => {
+    return seconds + 60 * minutes + 3600 * hours
+}
+
+console.log(toSeconds(4, 22, 45))
+
+const weekDay = (day) => {
+    switch (day) {
+        case 7:
+            return "sunnuntai"
+        case 6:
+            return "lauantai"
+        case 5:
+            return "perjantai"
+        case 4:
+            return "torstai"
+        case 3:
+            return "keskiviikko"
+        case 2:
+            return "tiistai"
+        case 1:
+            return "maanantai"
+        default:
+            return "en tunnista päivää"
+    }
+
+}
+
+console.log(weekDay(9))
