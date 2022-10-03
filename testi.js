bd334af461b2c514961f481c7fb13a7b95e2ed5c
heikki-nieminen <heikki.nieminenn@gmail.com>
2022-10-03 13:41:57 +0300

    Testi

---
+++ testi.js
@@ -0,0 +1,9 @@
+const biggerThanHundred = (number) => {
+    number = 101
+    if (biggerThanHundred > 100) {
+        return "Syötit luvun, joka on suurempi kuin 100"
+    }
+    else {
+        return ""
+    }
+}
